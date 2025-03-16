0a424a75ccad0518024c8080723a76502cedb2aa
Lionel053 <ledison@unb.ca>
2025-03-16 18:00:28 -0300

    Updated main routines

--- Student.h
+++ Student.h
@@ -1,18 +1,22 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-#define MAX_CHARS_NAME 50
+#define ID_LENGTH 8
 
 typedef struct Student {
-
+//TODO
 }Student;
 
 //TODO Student specific routines as specified in the UML diagram
 
+
 //main routines
-Student** init_student_data(char** filename);
-void create_student_routine(Student** list);
-void view_student_routine(Student** list);
-void delete_student_routine(Student** list);
+Student** init_student(char* filename);
+void create_student(Student** list);
+void view_student(Student** list);
+void delete_student(Student** list);
+void edit_student(Student** list);
+void view_list(Student** list);
+
 
 #endif
 
--- main.c
+++ main.c
@@ -4,44 +4,53 @@
 
 int main(int argc, char **argv) {
 
-    char** filename = "student_info.csv";
-    Student** list = init_student_data(filename);
+    char* filename = "student_info.csv";
+    Student** list = init_student(filename);
+    /*
     if (list == NULL) {
-        perror("Failed connnecting to database, contact your IT admin for absolutely no help\n");
+        printf("Failed connnecting to database, contact your IT admin for absolutely no help\n");
         return EXIT_FAILURE;
     }
-
+    */
     int choice;
 
     while (1) {
 
         printf("Welcome to the Grade Management System!\n");
         printf("Please choose from one of the following options:\n");
-        printf("1: Add a new Student\n");
+        printf("1: Add a new student\n");
         printf("2: View info for an existing Student\n");
-        printf("3: Delete student data\n");
-        printf("4: Exit\n");
+        printf("3: Modify student data\n");
+        printf("4: Delete existing student\n");
+        printf("5: View complete list of students\n");
+        printf("6: Exit\n");
         
         if (scanf("%d", &choice) != 1) {
-            printf("Invalid entry, try another selection");
+            printf("Invalid entry, try another selection\n");
             while (getchar() != '\n');
             continue;
         }
 
         switch(choice) {
             case 1:
-                create_student_routine(list);
+                create_student(list);
                 break;
             case 2:
-                view_student_routine(list);
+                view_student(list);
                 break;
             case 3:
-                delete_student_routine(list);
+                edit_student(list);
                 break;
             case 4:
+                delete_student(list);
+                break;
+            case 5:
+                view_list(list);
+                break;
+            case 6:
                 return EXIT_SUCCESS;
             default:
-                printf("Invalid entry, try another selection");
+                printf("Invalid entry, try another selection\n");
                 continue;
         }
             
