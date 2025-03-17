cfd21978a8c41604fca6e3406d29c66a96b657de
dhdhdhdhdhdhdhdhdhdhdhdhdh <d.h@unb.ca>
2025-03-16 21:04:18 -0300

    Student structure defined. Some additional functions defined.

---
+++ student.h
@@ -0,0 +1,24 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#define MAX_NAME 100
+#define MAX_GRADES 50
+#define MAX_STUDENTS 100
+
+typedef struct {
+    int id;
+    char *name;
+    float *grades;
+    float gpa;
+} Student;
+
+// main functions
+void load_file(char *filename, Student **students, int *studentCount);
+void save_file(char *filename, Student **students, int *studentCount);
+void add_student(Student **students, int *studentCount);
+void delete_student(Student **students);
+void edit_student(Student **students);
+void view_student(Student **students);
+void view_list(Student **students, int studentCount);
+
+#endif
